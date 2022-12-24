a09214bd7fe99531cde34d788a579b5d942aaed7
Mufachir Hossain <mufachirhossain@gmail.com>
2022-12-24 10:27:37 +0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import logo2 from './5692030.png';
 import './App.css';
+
+
+const elem = <div className="App">
+              <header className="App-header">
+                <img src={logo2} className="App-logo" alt="logo" />
+                <p>
+                  Take part in quiz | Enhance your skill &amp; knowledge.
+                </p>
+                <a className="App-link" href="http://mufachir.me" target="_blank" rel="noopener noreferrer">
+                  Bismillah. Started Learning React.
+                </a>
+              </header>
+            </div>
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    elem
   );
 }
 
