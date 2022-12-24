bbfd6523e3f7d20ff5b5e347356f679f5f6d8c04
Mufachir Hossain <mufachirhossain@gmail.com>
2022-12-24 16:54:31 +0600

    ..added-bootstrap..

--- src/App.js
+++ src/App.js
@@ -2,16 +2,41 @@ import logo2 from './5692030.png';
 import './App.css';
 
 
-const elem = <div className="App">
-              <header className="App-header">
-                <img src={logo2} className="App-logo" alt="logo" />
-                <p>
-                  Take part in quiz | Enhance your skill &amp; knowledge.
-                </p>
-                <a className="App-link" href="http://mufachir.me" target="_blank" rel="noopener noreferrer">
-                  Bismillah. Started Learning React.
-                </a>
-              </header>
+const elem = <div classNameName="App">
+              <nav className="navbar navbar-expand-lg bg-light">
+                <div className="container-fluid">
+                  <a className="navbar-brand" href="/">TextUtils</a>
+                  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                  </button>
+                  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                      <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="/">Home</a>
+                      </li>
+                      <li className="nav-item">
+                        <a className="nav-link" href="/">Link</a>
+                      </li>
+                      <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                          Dropdown
+                        </a>
+                        <ul className="dropdown-menu">
+                          <li><a className="dropdown-item" href="/">Action</a></li>
+                          <li><a className="dropdown-item" href="/">Another action</a></li>
+                          <li><hr className="dropdown-divider"/></li>
+                          <li><a className="dropdown-item" href="/">Something else here</a></li>
+                        </ul>
+                      </li>
+                     
+                    </ul>
+                    <form className="d-flex" role="search">
+                      <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                      <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                  </div>
+                </div>
+              </nav>
             </div>
 
 
